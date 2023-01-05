5cf0ec842e6c3a62e4f13623dc514cb92b333584
zanni <118340911+kCMI113@users.noreply.github.com>
2023-01-05 21:51:49 +0900

    [unrated] Title: 햄버거 만들기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/133502. 햄버거 만들기/햄버거 만들기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> ingredient) {
+    int answer = 0;
+    vector<int> burger ={1,2,3,1};
+    vector<int> check(4);
+    for(int i=0; i+4<=ingredient.size(); i++){
+        copy(ingredient.begin()+i,ingredient.begin()+i+4, check.begin());
+        if(check==burger) {
+            answer++;
+            ingredient.erase(ingredient.begin()+i,ingredient.begin()+i+4);
+            i-=4;
+        }
+    }
+    //##
+    return answer;
+}
