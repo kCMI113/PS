fb6cdb1bad268399c247f96cbc00947a2006d9c0
zanni <118340911+kCMI113@users.noreply.github.com>
2023-03-27 18:37:22 +0900

    코드 변경으로 인한 수정

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -1,20 +1,11 @@
 N = int(input())
 km = list(map(int,input().split()))
 cost = list(map(int,input().split()))
-now_oil = 0
 tot_cost = 0
+min_cost = cost[0]
 
-for i in range(N-1):
-    if(now_oil < km[i]):
-        if(cost[i] == min(cost[i:])):
-            tot_cost += cost[i]*sum(km[i:])
-            break
-        elif(i+1<N and cost[i]<cost[i+1]):
-            tot_cost += cost[i]*(km[i]+km[i+1])
-            now_oil += (km[i]+km[i+1])
-        else:
-            tot_cost += cost[i]*km[i]
-            now_oil += km[i]
-    now_oil -= km[i]
+for i in range(len(km)):
+    min_cost = min(min_cost, cost[i])
+    tot_cost += min_cost*km[i]
     
 print(tot_cost)
