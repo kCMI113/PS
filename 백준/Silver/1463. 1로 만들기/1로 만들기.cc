f1feb88cdbafbfa58a3ebd394a5ecfd9123753a5
zanni <118340911+kCMI113@users.noreply.github.com>
2023-03-28 19:02:15 +0900

    [Silver III] Title: 1로 만들기, Time: 8 ms, Memory: 8176 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -2,16 +2,18 @@
 using namespace std;
 
 int main(void){
-    int n;
+    int n, store;
     vector<int> res = {0,0};
     cin>>n;
 
     for(int i=2; i<=n; i++){
-        vector<int> store = {res[i-1] + 1};
-        if(i%2 == 0) store.push_back(res[i/2]+1);
-        if(i%3 == 0) store.push_back(res[i/3]+1);
-        res.push_back(*min_element(store.begin(),store.end()));
+		store = res[i-1] + 1;
+        if(i%2 == 0) store = min(store,res[i/2]+1);
+        if(i%3 == 0) store = min(store,res[i/3]+1);
+        
+        res.push_back(store);
     }
     cout<<res.back();
+    
     return 0;
 }
