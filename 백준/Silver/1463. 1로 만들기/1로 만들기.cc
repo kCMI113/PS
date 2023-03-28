772d7b0ef28155fef105d79de2704162e4859c3b
zanni <118340911+kCMI113@users.noreply.github.com>
2023-03-28 19:05:19 +0900

    indentation

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -7,7 +7,7 @@ int main(void){
     cin>>n;
 
     for(int i=2; i<=n; i++){
-		store = res[i-1] + 1;
+	store = res[i-1] + 1;
         if(i%2 == 0) store = min(store,res[i/2]+1);
         if(i%3 == 0) store = min(store,res[i/3]+1);
         
