5d0ea1aee4414cf6f43219e2a408ab80ea34c6c3
zanni <118340911+kCMI113@users.noreply.github.com>
2023-04-04 10:09:44 +0900

    [Silver II] Title: 랜선 자르기, Time: 72 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,22 @@
+import sys
+
+k, n = map(int, input().split())
+cable = []
+
+for i in range(k):
+    cable.append(int(sys.stdin.readline()))
+
+start, end = 1,max(cable)
+new_n = 0
+max_len = 0
+
+while(start<=end):
+    mid = (start+end)//2
+    new_n = sum(map(lambda num:num//mid, cable))
+    if new_n >= n:
+        max_len = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+        
+print(max_len)
