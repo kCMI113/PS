21a5c3562c224fd4d51d6e4897a6e2054a9be9d0
zanni <118340911+kCMI113@users.noreply.github.com>
2023-06-19 03:27:48 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 3120 ms, Memory: 57744 KB -BaekjoonHub

--- 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.py
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.py
@@ -1,46 +1,46 @@
 import sys
 
-def init(nums, tree, node, start, end, oper):
+def init(nums, tree, node, start, end):
     if start == end:
         # init leaf node
-        tree[node] = nums[start]
+        tree[node] = [nums[start], nums[start]] # [min, max]
     else:
         # init child node first
-        init(nums, tree, node*2, start, (start+end)//2, oper)
-        init(nums, tree, node*2+1, (start+end)//2+1, end, oper)
+        init(nums, tree, node*2, start, (start+end)//2)
+        init(nums, tree, node*2+1, (start+end)//2+1, end)
         # then, init present node by using L,R child node
-        tree[node] = oper(tree[node*2], tree[node*2+1])
+        tree[node] = [min(tree[node*2][0], tree[node*2+1][0]), max(tree[node*2][1], tree[node*2+1][1])]
 
-def query(tree, node, start, end, left, right, oper):
+def query(tree, node, start, end, left, right):
     # want to get total sum of [L,R] sub part
+    res = []
 
     if end < left or start > right:
-        return -1
+        return [-1, -1]
     elif left <= start and end <= right:
         return tree[node]
     
-    L_res = query(tree, node*2, start, (start+end)//2, left, right, oper)
-    R_res = query(tree, node*2+1, (start+end)//2+1, end, left, right, oper)
-    if L_res < 0:
-        return R_res
-    if R_res < 0:
-        return L_res
-    
-    return oper(R_res, L_res)
+    L_res = query(tree, node*2, start, (start+end)//2, left, right)
+    R_res = query(tree, node*2+1, (start+end)//2+1, end, left, right)
 
+    if L_res[0]*R_res[0] < 0:
+        res.append(max(L_res[0], R_res[0]))
+    else:
+        res.append(min(L_res[0], R_res[0]))
+    
+    res.append(max(L_res[1], R_res[1]))
+    
+    return res
         
 N, M = map(int, sys.stdin.readline().split())
 nums = [int(sys.stdin.readline()) for _ in range(N)]
-min_tree = [-1]*(4*N)
-max_tree = [-1]*(4*N)
+tree = [0,0]*(4*N)
 
 # init tree
-init(nums, min_tree, 1, 0, N-1, min)
-init(nums, max_tree, 1, 0, N-1, max)
-
+init(nums, tree, 1, 0, N-1)
 
 for i in range(M):
     num1, num2 = map(int, sys.stdin.readline().split())
-
-    print(query(min_tree, 1, 0, N-1, num1-1, num2-1, min), end=" ")
-    print(query(max_tree, 1, 0, N-1, num1-1, num2-1, max))
+    res = query(tree, 1, 0, N-1, num1-1, num2-1)
+    print(res[0], end=" ")
+    print(res[1])
