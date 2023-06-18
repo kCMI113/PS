b10e4a3c138a8206d91dc5751fe33afc4b8dda30
zanni <118340911+kCMI113@users.noreply.github.com>
2023-06-19 03:45:08 +0900

    [Gold I] Title: 최솟값과 최댓값, Time: 2932 ms, Memory: 54620 KB -BaekjoonHub

--- 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.py
+++ 백준/Gold/2357. 최솟값과 최댓값/최솟값과 최댓값.py
@@ -15,19 +15,17 @@ def query(tree, node, start, end, left, right):
     # want to get total sum of [L,R] sub part
 
     if end < left or start > right:
-        return [float('inf'), 0]
+        return [1e9+1, 0]
     elif left <= start and end <= right:
         return tree[node]
     
     L_res = query(tree, node*2, start, (start+end)//2, left, right)
     R_res = query(tree, node*2+1, (start+end)//2+1, end, left, right)
     return [min(L_res[0], R_res[0]), max(L_res[1], R_res[1])]
-
-
         
 N, M = map(int, sys.stdin.readline().split())
 nums = [int(sys.stdin.readline()) for _ in range(N)]
-tree = [[float('inf'),0]]*(4*N)
+tree = [[1e9+1,0]]*(4*N)
 
 # init tree
 init(nums, tree, 1, 0, N-1)
