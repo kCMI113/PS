c17141d5730d84ddd36e651d02850590c018ea62
zanni <118340911+kCMI113@users.noreply.github.com>
2023-08-09 17:31:33 +0900

    [Bronze II] Title: 팰린드롬인지 확인하기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.py
@@ -1,11 +1,8 @@
 import sys
 
 query = list(sys.stdin.readline().rstrip())
-len = len(query)
-half_len = len//2
-cnt = 0
 
-for idx in range(half_len):
-    cnt += (query[idx] == query[len-idx-1])
-
-print(int(cnt == len//2))
+if query[::-1] == query:
+    print(1)
+else:
+    print(0)
