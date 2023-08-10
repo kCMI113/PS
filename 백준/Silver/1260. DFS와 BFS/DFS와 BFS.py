d03ba294a3074754c6dea47d3943c471521190a7
zanni <118340911+kCMI113@users.noreply.github.com>
2023-08-10 14:34:05 +0900

    [Silver II] Title: DFS와 BFS, Time: 88 ms, Memory: 34176 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -5,16 +5,15 @@ def dfs(node: int) -> None:
     if visit[node]:
         return
     
-    dfs_res.append(node)
+    print(node, end=" ")
     visit[node] = 1
 
     for next in adj_matrix[node]:
         dfs(next)
 
 def bfs(node: int) -> None:
-    que = deque()
-    que.append(node)
-    bfs_res.append(node)
+    que = deque([node])
+    print(node, end=" ")
     visit[node] = 1
 
     while que:
@@ -23,25 +22,23 @@ def bfs(node: int) -> None:
             if not visit[next]:
                 que.append(next)
                 visit[next] = 1
-                bfs_res.append(next)
+                print(next, end=" ")
 
 N, M, V = map(int, sys.stdin.readline().rstrip().split())
 adj_matrix = [[] for _ in range(N+1)]
-dfs_res = []
-bfs_res = []
+
 
 for _ in range(M):
     a,b = map(int, sys.stdin.readline().rstrip().split())
     adj_matrix[a].append(b)
     adj_matrix[b].append(a)
-    adj_matrix[a] = sorted(adj_matrix[a])
-    adj_matrix[b]= sorted(adj_matrix[b])
+    adj_matrix[a].sort()
+    adj_matrix[b].sort()
 
 
 visit = [0 for _ in range(N+1)]
 dfs(V)
-print(" ".join(map(str, dfs_res)))
+print('')
 
 visit = [0 for _ in range(N+1)]
 bfs(V)
-print(" ".join(map(str, bfs_res)))
