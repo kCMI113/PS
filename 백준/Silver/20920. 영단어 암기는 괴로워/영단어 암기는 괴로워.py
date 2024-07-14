db3c9c7592cdc8a405073f9bdee289496d4be8d9
zanni <118340911+kCMI113@users.noreply.github.com>
2024-07-15 06:34:11 +0900

    [Silver III] Title: 영단어 암기는 괴로워, Time: 192 ms, Memory: 51728 KB -BaekjoonHub

--- 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
+++ 백준/Silver/20920. 영단어 암기는 괴로워/영단어 암기는 괴로워.py
@@ -1,25 +1,11 @@
 import sys
+from collections import Counter
 
 input = sys.stdin.readline
 N, M = map(int, input().split())
 
-words = {}
-info = []
-idx = 0
-
-for _ in range(N):
-    word = input().rstrip()
-    ele1 = -1
-    ele2 = len(word)
-    if ele2 < M:
-        continue
-    if word not in words.keys():
-        words[word] = idx
-        idx += 1
-        info.append([ele1, -1 * ele2, word])
-    else:
-        info[words[word]][0] -= 1
-
-res = sorted(info)
-for w in res:
-    print(w[-1])
+words = [input().rstrip() for _ in range(N)]
+cnter = Counter(word for word in words if len(word) >= M)
+words = sorted(list(cnter))
+words.sort(key=lambda x: [cnter.get(x), len(x)], reverse=True)
+print("\n".join(words))
